db85070f3968d090ce904a76976611596de7e1bc
roma199627 <roma1996revun@gmail.com>
2023-01-26 04:18:14 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredient = document.getElementById("ingredients")
+
+const liElem = document.createElement("li")
+
+ingredient.forEach((ingredient) => {
+  const liElem = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  document.querySelector("#ingredient").prepend(item);
+});
