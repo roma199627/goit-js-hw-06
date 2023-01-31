bce1aa9f2922637cb36275d8c7da527dbac1a448
roma199627 <roma1996revun@gmail.com>
2023-02-01 01:56:12 +0200

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,6 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const gallery = document.querySelector("gallery")
+images.map(image =>
+<li><img src=" ${image.url}" alt="${image.alt}"></li>)
