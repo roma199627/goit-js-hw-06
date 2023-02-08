8ba02e67eeca49d5d9e0b067f39355c243b7b512
roma199627 <roma1996revun@gmail.com>
2023-02-09 01:29:25 +0200

    Update task-05.js

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -6,4 +6,12 @@
 
 const text = document.querySelector('#name-input')
 const output = document.querySelector('#name-output')
-text.output = function ()
+text.output = function ()   {
+    if ('' === text  ){ 
+      input =  'Please enter your name';
+    }
+ else {
+  input = ' Anonymous';
+}
+ }
+
